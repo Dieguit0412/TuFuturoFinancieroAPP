486abb2b2147c460709a8596f250a23869a6174f
Dieguit0412 <diegocha04@gmail.com>
2024-06-01 21:09:00 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tufuturofinancieroapp/MainActivity2.kt
@@ -0,0 +1,49 @@
+package com.example.tufuturofinancieroapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2() : AppCompatActivity(), Parcelable {
+    constructor(parcel: Parcel) : this()
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MainActivity2> {
+        override fun createFromParcel(parcel: Parcel): MainActivity2 {
+            return MainActivity2(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MainActivity2?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets }
+
+
+
+        val imgButton: ImageButton = findViewById(R.id.Tips)
+        imgButton.setOnClickListener{
+            val intent: Intent = Intent (this, MainActivity3::class.java)
+            startActivity(intent)}}}
+
